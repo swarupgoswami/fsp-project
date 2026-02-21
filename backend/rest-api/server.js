4d8d6f94b447886b19c1aadc9a5ef5087f73eecb
swarup Goswami <goswamiswarup97@gmail.com>
2026-02-21 19:11:01 +0530

    server.js file

---
+++ backend/rest-api/server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const dotenv=require('dotenv');
+dotenv.config();
+const {connectDB}=require('./config/db');
+const User=require('./model/userSchema');
+
+connectDB();
+
+const app=express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('hhhhh');
+});
+
+console.log("POST route registered");
+app.post('/create',async(req,res)=>{
+    console.log("POST /create route hit");
+    const {name}=req.body;
+    const user=await User.create({name});
+    res.json(user);
+})
+app.listen(3000,()=>{
+    console.log("server is running ");
+})
